90b4209863d8996a22ea72f376f2bffb0595540a
ys012313 <jshking7177@naver.com>
2020-02-23 17:00:01 +0900

    views 수정

--- history/views.py
+++ history/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from . import models
+from . import serializers
 
 # Create your views here.
+
+class History_View(viewsets.ModelViewSet):
+    queryset = models.History.objects.all()
+    serializer_class = serializers.HistorySerializer
