0fca1f07c391b7b0f1b42d07aa4fb5b1c7505cfb
devdw98 <devdw98@gmail.com>
2020-02-23 12:23:13 +0900

    image view

--- image/views.py
+++ image/views.py
@@ -1,1 +1,9 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from . import models
+from . import serializers
+
+# Create your views here.
+class Image_View(viewsets.ModelViewSet):
+    queryset = models.Image.objects.all()
+    serializer_class = serializers.ImageSerializer
