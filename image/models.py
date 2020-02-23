558654cfd2a43130a17b071f75e946df9d6d7775
devdw98 <devdw98@gmail.com>
2020-02-23 12:20:36 +0900

    image model

--- image/models.py
+++ image/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Image(models.Model):
+    id_image = models.AutoField(primary_key=True)
+    file_image = models.TextField()
+    name_image = models.CharField(max_length=256)
+
+    class Meta:
+        managed = False
+        db_table = 'image'
+
+    def __str__(self):
+        return self.name_image
