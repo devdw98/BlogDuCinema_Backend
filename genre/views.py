787758d0b302c162b265dfbdb3536198ba263ec8
jeongin <jeongiin@naver.com>
2020-02-23 13:56:31 +0900

    views 수정

--- genre/views.py
+++ genre/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
-
+from rest_framework import viewsets
+from . import models
+from . import serializers
 
 # Create your views here.
+
+class Genre_View(viewsets.ModelViewSet):
+    queryset = models.Genre.objects.all()
+    serializer_class = serializers.GenreSerializer
