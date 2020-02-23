9cd05e300a4c15ec77c55bdc73b08008c16be65a
ys012313 <jshking7177@naver.com>
2020-02-23 16:28:00 +0900

    models 수정

--- history/models.py
+++ history/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class History(models.Model):
+    id_history = models.AutoField(primary_key=True)
+    title_history = models.CharField(max_length=45, blank=True, null=True)
+    content_history = models.TextField()
+
+    class Meta:
+        managed = False
+        db_table = 'history'
+
+    def __str__(self):
+        return self.title_history
